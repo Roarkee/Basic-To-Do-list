fbb48f82ed22793e89e10cfafb1d210716edaaa6
Roarkee <henokammaccarthy10@gmail.com>
2024-09-27 15:44:24 +0000

    using a database in the app

--- app.py
+++ app.py
@@ -1,39 +1,65 @@
 from flask import Flask, render_template,redirect,request,url_for
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 app=Flask(__name__,template_folder="templates")
-todos=[{'task':'hello', 'status':False}]
+app.config[ 'SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
+app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+
+class Todo(db.Model):
+
+    task_id=db.Column(db.Integer, primary_key=True )
+    task=db.Column(db.String, nullable=False)
+    status=db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
+
+
+
+# todos=[{'task':'hello', 'status':False}]
 @app.route("/")
 def index():
+    todos=Todo.query.all()
     return render_template("index.html", todos=todos) 
 
 @app.route("/add", methods=['POST'])
 def add():
-    todo=request.form['task']
-    todos.append({'task':todo, 'status':False})
-    return redirect(url_for('index'))
-
-
-@app.route("/edit/<int:index>" ,methods=["POST","GET"])
-def edit(index):
-    todo=todos[index]
-    if request.method=='POST':
-        todo['task']=request.form['task']
-        return redirect(url_for('index'))
-    else:
-        return render_template("edit.html", todo=todo, index=index)
+    todo=request.form.get('task')
+    new_todo=Todo(task=todo, status=False)
+    db.session.add(new_todo)
+    db.session.commit()
     
-@app.route("/check/<int:index>")
-def check(index):
-    todos[index]['status']=not todos[index]['status']
+
+    # todos.append({'task':todo, 'status':False})
     return redirect(url_for('index'))
 
 
-@app.route("/delete/<int:index>")
-def delete(index):
-    del todos[index]
+@app.route("/edit/<int:task_id>" ,methods=["POST","GET"])
+def edit(task_id):
+    todo=Todo.query.get(task_id)
+    todo.status=not todo.status
+    db.session.commit()
+    # todo=todos[index]
+    # if request.method=='POST':
+    #     todo['task']=request.form['task']
+    #     return redirect(url_for('index'))
+    # else:
+    return redirect(url_for("index.html"))
+    
+# @app.route("/check/<int:index>")
+# def check(index):
+#     todos[index]['status']=not todos[index]['status']
+#     return redirect(url_for('index'))
+
+
+@app.route("/delete/<int:task_id>")
+def delete(task_id):
+    todo=Todo.query.get(task_id)
+    db.session.delete(todo)
+    db.session.commit()
     return redirect(url_for('index'))
 
 if __name__=="__main__":
